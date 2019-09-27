b2cc479025c3c3f5f3a85082dfe923d3ee31cf89
mtervohe <matthew.tervo@jivesoftware.com>
2019-09-27 15:28:39 -0700

    added support for querying for specific car id. Also fixed a bug in CarRepository where the id was incorrectly set to Integer instead of the String id UUID that it is.

--- src/main/java/com/cardealership/dao/CarRepository.java
+++ src/main/java/com/cardealership/dao/CarRepository.java
@@ -4,4 +4,4 @@ import com.cardealership.dao.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CarRepository extends JpaRepository<Car, Integer>, JpaSpecificationExecutor<Car> { }
+public interface CarRepository extends JpaRepository<Car, String>, JpaSpecificationExecutor<Car> { }
--- src/main/java/com/cardealership/web/controller/CarController.java
+++ src/main/java/com/cardealership/web/controller/CarController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 //todo - remove this workaround to allow frontend to make requests
 @CrossOrigin(origins = {"http://localhost:3000", "http://localhost"}, maxAge = 3600)
@@ -24,11 +26,11 @@ public class CarController {
     @Autowired
     RequestParamResolver paramResolver;
 
-    @GetMapping(path = "/carsall")
-    public List<Car> getAllCars() {
-        List<Car> cars = new ArrayList<>();
-        carRepository.findAll().forEach(cars::add);
-        return cars;
+    @GetMapping(path = "/cars/all")
+    public List<Car> getCarsAll() {
+        List<Car> results = new ArrayList<>();
+        carRepository.findAll().forEach(results::add);
+        return results;
     }
 
     @GetMapping(path = "/cars")
@@ -36,5 +38,17 @@ public class CarController {
                             @RequestParam(value = "search") String search) {
         Specification<Car> spec = paramResolver.resolveSpecification(exclusive, search);
         return carRepository.findAll(spec);
+    }
+
+    @GetMapping(path = "/cars/{id}")
+    public List<Car> getCarById(@PathVariable(value = "id") String id) {
+        List<Car> results = new ArrayList<>();
+        if(!id.isEmpty()) {
+            Optional<Car> car = carRepository.findById(id);
+            if(car.isPresent()) {
+                results.add(car.get());
+            }
+        }
+        return results;
     }
 }
